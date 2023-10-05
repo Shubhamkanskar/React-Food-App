244cc17ef439f4d07290d3276bf8a42817c90d82
shubham <shubhamkanaskar75@gmail.com>
2023-10-05 21:15:35 +0530

    Food-app

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Layout from "../../Components/layouts/Layout";
+import Section1 from "./Section1";
+import "../../styles/HomeStyle.css";
+
+const Home = () => {
+  return (
+    <>
+      <Layout>
+        <Section1></Section1>
+      </Layout>
+    </>
+  );
+};
+
+export default Home;
